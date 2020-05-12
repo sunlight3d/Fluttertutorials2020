ac46c491a1197663268ce105f727bd125fda9d3b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-05-12 23:04:43 +0700

    update

--- 23/myappTemp/lib/foods_page.dart
+++ 23/myappTemp/lib/foods_page.dart
@@ -20,11 +20,62 @@ class FoodsPage extends StatelessWidget {
         child: ListView.builder(
           itemCount: foods.length,
           itemBuilder: (context, index) {
-            Food food = foods[index];
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              clipBehavior: Clip.hardEdge,
-              child: Image.network(food.urlImage),
+            Food food = foods[index];          
+            return Stack(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    clipBehavior: Clip.hardEdge,
+                    child: Image.network(food.urlImage),
+                  ),),
+                  Positioned(
+                    top: 30,
+                    left: 30,
+                    child: Container(
+                      padding: EdgeInsets.all(5),                      
+                      decoration: BoxDecoration(
+                        color: Colors.black45,
+                        borderRadius: BorderRadius.circular(10),                                                                      
+                        border: Border.all(color: Colors.white, width: 2)
+                      ),
+                      child: Row(                                            
+                      children: <Widget>[
+                        Icon(Icons.timer, color: Colors.white, size: 25,),
+                        SizedBox(width: 5,),
+                        Text('${food.duration.inMinutes} minutes',style: TextStyle(fontSize: 22, color: Colors.white),),
+                      ],)
+                    )
+                  ),
+                  Positioned(
+                    top: 30,
+                    right: 30,
+                    child: Container(
+                      padding: EdgeInsets.all(7),                      
+                      decoration: BoxDecoration(
+                        color: Colors.greenAccent,
+                        borderRadius: BorderRadius.circular(10),                                                                                              
+                      ),
+                      child: Row(                                            
+                      children: <Widget>[                                                
+                        Text('${food.complexity.toString().split('.').last}',style: TextStyle(fontSize: 22, color: Colors.black),),
+                      ],)
+                    )
+                  ),
+                  Positioned(                    
+                    bottom: 30,
+                    right: 30,
+                    child: Container(                      
+                      child: Text('${food.name}',style: TextStyle(fontSize: 30, color: Colors.white),),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.black45,
+                        borderRadius: BorderRadius.circular(10)                      
+                      ),
+                    )
+                  )
+              ]
             );
           }
         ),
