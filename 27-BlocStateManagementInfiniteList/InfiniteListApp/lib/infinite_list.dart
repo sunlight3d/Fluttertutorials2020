e87db7f51979c4c51015bd42e2bbaab9a68db535
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-06-28 13:44:25 +0700

    update

--- 27-BlocStateManagementInfiniteList/InfiniteListApp/lib/infinite_list.dart
+++ 27-BlocStateManagementInfiniteList/InfiniteListApp/lib/infinite_list.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'models/comment.dart';
 
 class InfiniteList extends StatelessWidget {
   @override
@@ -8,10 +10,29 @@ class InfiniteList extends StatelessWidget {
     //we use JSONPlaceHolder for testing
     //https://jsonplaceholder.typicode.com/comments?_start=1&_limit=20
     //page 1, 20 items
+    //Now test
+    _getCommentsFromApi(0, 20);
     return Scaffold(
       body: SafeArea(
         child: Text('Infinite List using Bloc', style: TextStyle(fontSize: 30),),
       ),
     );
+  }
+  _getCommentsFromApi(int page, int limit) {
+    final url = 'https://jsonplaceholder.typicode.com/comments?_start=$page&_limit=$limit';
+    http.Client().get(url).then((response) {
+      if(response.statusCode == 200) {
+        final responseData = json.decode(response.body) as List;
+        final List<Comment> comments = responseData.map((element){
+          return Comment(
+            id: element['id'],
+            name: element['name'],
+            email: element['email'],
+            body: element['body']
+          );
+        }).toList();
+        print('haha');
+      }
+    });
   }
 }
---
+++ 27-BlocStateManagementInfiniteList/InfiniteListApp/lib/models/comment.dart
@@ -0,0 +1,7 @@
+class Comment {
+  final int id;
+  final String name;
+  final String email;
+  final String body;
+  Comment({this.id, this.name, this.email, this.body});
+}
