ac952a6785d6dc28c4bf5eb6af18116b66756cae
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-07-22 16:19:37 +0700

    update

--- 34-44BlocManagementFirebaseLogin/firebaseLoginBloc/lib/repositories/user_repository.dart
+++ 34-44BlocManagementFirebaseLogin/firebaseLoginBloc/lib/repositories/user_repository.dart
@@ -6,10 +6,17 @@ class UserRepository {
   final FirebaseAuth _firebaseAuth;
   final GoogleSignIn _googleSignIn;
   //constructor
+
   UserRepository({FirebaseAuth firebaseAuth, GoogleSignIn googleSignIn}):
       _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance,
-      _googleSignIn = googleSignIn ?? GoogleSignIn();
+      _googleSignIn = googleSignIn ?? GoogleSignIn(scopes: <String>[
+        'email',
+        'https://www.googleapis.com/auth/contacts.readonly',
+      ]);
   Future<FirebaseUser> signInWithGoogle() async {
+//    await _googleSignIn.signIn();
+//    FirebaseUser firebaseUser = await _firebaseAuth.currentUser();
+//    return firebaseUser;
     final GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
     final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
     final AuthCredential authCredential = GoogleAuthProvider.getCredential(
@@ -17,7 +24,8 @@ class UserRepository {
         accessToken: googleSignInAuthentication.idToken
     );
     await _firebaseAuth.signInWithCredential(authCredential);
-
+    final currentUser = _firebaseAuth.currentUser();
+    return currentUser;
   }
   Future<void> signInWithEmailAndPassword(String email, String password) async{
     return await _firebaseAuth.signInWithEmailAndPassword(
